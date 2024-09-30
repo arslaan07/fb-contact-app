cdd797a306c89c53fc3f39cc26fc11f7b1680b33
arslaan07 <arslaanbanday5@gmail.com>
2024-09-30 20:51:11 +0530

    .env added

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,19 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDnOFmcGIIRneRMjO7Z1RbO2Di-ESX21_0",
-  authDomain: "contacts-app-f9822.firebaseapp.com",
-  projectId: "contacts-app-f9822",
-  storageBucket: "contacts-app-f9822.appspot.com",
-  messagingSenderId: "609974596936",
-  appId: "1:609974596936:web:c567ed147cdc7512636f7d"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
