4ddc6c20d1eb8381e6db76367c1b68d32981e307
arslaan07 <arslaanbanday5@gmail.com>
2024-10-01 02:11:39 +0530

    auth files added

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+  return (
+    <div className='w-[361px] min-h-[200px] mx-auto bg-white p-2
+    rounded-md '>
+        <h1 className='text-2xl font-medium text-center mb-2'>Login</h1>
+      <form className='p-3'>
+        <div className='flex flex-col'>
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' placeholder='email' 
+            className='border py-1 px-2 outline-none rounded-md shadow'/>
+            <p className='h-[15px] text-red-600'></p>
+        </div>
+        <div className='flex flex-col'>
+            <label htmlFor="password">Password</label>
+            <input type="password" id='password' placeholder='password'
+             className='border py-1 px-2 outline-none rounded-md shadow'/>
+            <p className='h-[15px] text-red-600'></p>
+        </div>
+        <input type="submit" value="Submit"
+         className='bg-blue-500 text-white px-2 
+         py-1 font-medium rounded-lg shadow mt-2' />
+      </form>
+      <div className='flex justify-end gap-2 text-sm font-medium'>
+        <p className='text-zinc-500'>New user </p>
+        <Link to="/login" className='text-blue-600'>Register Here</Link>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Login
