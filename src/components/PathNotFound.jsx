c9f6bc658cdffe10ca3fd4f27f2c6393af30406a
arslaan07 <arslaanbanday5@gmail.com>
2024-10-03 21:59:20 +0530

    fix: go back button included for 404 path

--- src/components/PathNotFound.jsx
+++ src/components/PathNotFound.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import { MdArrowBack } from "react-icons/md";
 const PathNotFound = () => {
   return (
     <div className='w-[361px]  mx-auto bg-white p-2
-    rounded-md flex justify-center '>
+    rounded-md flex flex-col justify-center items-center '>
       <h1 className='text-2xl font-medium'>404, Not Found !</h1>
+      <div className='flex text-xl mt-2'>
+        <MdArrowBack className='self-center mr-0.5 ' />
+        <Link to={"/"} className="font-medium">Go Back</Link>
+      </div>
+      
     </div>
   )
 }
